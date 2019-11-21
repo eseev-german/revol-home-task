1339995e700a12a8c76956b89ba7a0cc2a1b53ab
geseev <german.eseev@t-systems.com>
2019-11-21 16:34:45 +0300

    add high-level exception handling

---
+++ src/main/java/revol/home/task/controller/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package revol.home.task.controller;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ExceptionHandler implements ExceptionMapper<Exception> {
+    @Override
+    public Response toResponse(Exception exception) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                       .entity(exception.getMessage())
+                       .type(MediaType.TEXT_PLAIN_TYPE)
+                       .build();
+    }
+}
